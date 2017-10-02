01cddfcb53bc1dbed9c33ab131205bd35b1541da
Fyodor Ananiev <tedmx@ya.ru>
2017-10-02 16:14:16 +0300

    Errors on accessing properties (#12)
    Swallow errors on accessing properties

--- es6/snapshot.js
+++ es6/snapshot.js
@@ -16,7 +16,13 @@ function snapshotObject(obj, origRef, context) {
     if (ref.hasOwnProperty(key)) foundRefKeys++;
 
     const referenceStateForKey = ref[key];
-    const clonedStateForKey = snapshotHelper(obj[key], referenceStateForKey, context);
+    let value;
+    try {
+      value = obj[key];
+    } catch (e) {
+      // Ignore errors generated accessing properties returned by `Object.keys()`.
+    }
+    const clonedStateForKey = snapshotHelper(value, referenceStateForKey, context);
     clone[key] = clonedStateForKey;
     changed = changed || clonedStateForKey !== referenceStateForKey;
   }
